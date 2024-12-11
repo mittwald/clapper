80259106db373f8c05f35913ec1ed7d11076f1b7
Dirk007 <dirk@faustbande.de>
2024-12-11 08:29:07 +0100

    Simplify range

--- arg_parser.go
+++ arg_parser.go
@@ -99,7 +99,7 @@ func (pa *ArgsParser) Add(arg string) error {
 }
 
 func (pa *ArgsParser) PopTrailing(took int) *ArgsParser {
-	for _ = range took {
+	for range took {
 		if len(pa.Trailing) == 0 {
 			return pa
 		}
